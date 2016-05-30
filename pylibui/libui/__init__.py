6a5d197885fd8c57173de61351e6e8336ad311f6
João Ventura <joaojonesventura@gmail.com>
2016-05-30 17:56:11 +0100

    Detect platform

--- pylibui/libui/__init__.py
+++ pylibui/libui/__init__.py
@@ -3,13 +3,22 @@
 
 """
 
-import os
 import ctypes
+import os
+import platform
+
+
+if platform.system() == 'Darwin':
+    libname = 'libui.dylib'
+elif platform.system() == 'Windows':
+    libname = 'libui.dll'
+elif platform.system() == 'Linux':
+    libname = 'libui.so'
 
 
 CURR_PATH = os.path.dirname(os.path.realpath(__file__))
 SHARED_LIBS_PATH = os.path.join(CURR_PATH, 'sharedlibs')
-SHARED_LIBS = os.path.join(SHARED_LIBS_PATH, 'libui.dylib')
+SHARED_LIBS = os.path.join(SHARED_LIBS_PATH, libname)
 
 
 ctypes.cdll.LoadLibrary(SHARED_LIBS)
