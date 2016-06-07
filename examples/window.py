5049dca4928781ee8a9e5147f5f684e0755f2a24
mark-summerfield <mark@qtrac.eu>
2016-06-07 08:05:42 +0100

    If App is a context manager we don't need to call stop or close

--- examples/window.py
+++ examples/window.py
@@ -14,11 +14,12 @@ class MyWindow(Window):
         app.stop()
 
 
-app = App()
+def main():
+    with App():
+        window = MyWindow('Window', 800, 600)
+        window.setMargined(1)
+        window.show()
 
-window = MyWindow('Window', 800, 600)
-window.setMargined(1)
-window.show()
 
-app.start()
-app.close()
+if __name__ == "__main__":
+    main()
