c13abc214150d6e248160c6fbab0b7ebe9e85f2f
stonewell <jingnan.si@gmail.com>
2017-03-31 23:54:34 -0700

    update setup.py for OSX, using install path as rpath

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from distutils.core import setup, Extension
+import distutils
+import platform
+import os
+
+extra_link_args = []
+runtime_library_dirs = []
+if platform.system() == 'Darwin':
+    extra_link_args.append('-Wl,-rpath,' + os.path.join(distutils.sysconfig_get_python_lib(), 'pylibui'))
+elif platform.system() == 'Linux':
+    runtime_library_dirs.append('$ORIGIN')
 
 libui_module = Extension('pylibui._libui',
                              ['pylibui/libui.i'],
                              libraries=['ui'],
-                             runtime_library_dirs=['$ORIGIN']
+                             runtime_library_dirs=runtime_library_dirs,
+                             extra_link_args = extra_link_args
                              )
 
 
